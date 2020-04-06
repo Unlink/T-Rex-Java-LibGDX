09420e4250d4252afb0cf4711733cdff3f610b4a
Michal Ďuračík <miso2505993@gmail.com>
2020-04-06 13:55:13 +0200

    Inicializacia

---
+++ desktop/src/sk/uniza/fri/rex/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package sk.uniza.fri.rex.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import sk.uniza.fri.rex.RexGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RexGame(), config);
+	}
+}
